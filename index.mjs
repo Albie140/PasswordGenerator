5e1d338c6f00421600134b913021d7e29aa62f9b
Alberte Laventure <alberte.laventure@gmail.com>
2020-09-02 15:46:33 -0400

    updated all files

--- index.mjs
+++ index.mjs
@@ -1,4 +1,14 @@
-var articleDiv = document.
-querySelector ("#articles")
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-var font
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
